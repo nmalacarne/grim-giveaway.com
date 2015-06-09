e847ca485e8a94f70767bf490167f1fa13b56167
Nicholas Malacarne <nick@craterdust.com>
2015-06-09 13:56:15 -0400

    added 404

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,9 @@ var pages = require(__dirname + '/pages');
 
 module.exports = function routes(app) {
   app.use('/', pages);
+
+  // 404
+  app.use(function PageNotFound(req, res) {
+    res.redirect('/');
+  });
 };
