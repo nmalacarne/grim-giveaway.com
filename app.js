2919b7b3f56667add921cd6f9235ede4342d6e10
Nicholas Malacarne <nick@craterdust.com>
2015-06-09 13:51:00 -0400

    added connect-assets

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var app = require('express')();
-
-// view setup
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+var path    = require('path');
+var express = require('express');
+var app     = express();
 
 // environment
 app.set('port',     process.env.PORT      || 3000);
 app.set('logType',  process.env.LOG_TYPE  || 'dev');
+app.set('root',     process.env.ROOT      || __dirname);
+app.set('hostname', process.env.HOST_NAME || 'localhost:' + app.get('port'));
+
+// view setup
+app.set('views', path.join(app.get('root'), 'views'));
+app.set('view engine', 'jade');
 
 // middleware setup
-require(__dirname + '/middleware')(app);
+require(
+  path.join(app.get('root'), 'middleware')
+)(app);
 
 // routes setup
-require(__dirname + '/routes')(app);
+require(
+  path.join(app.get('root'), 'routes')
+)(app);
 
 // spin up the server
 app.listen(app.get('port'), function server() {
--- middleware/index.js
+++ middleware/index.js
@@ -1,10 +1,17 @@
-var assets  = require('connect-assets');
-var log     = require('morgan');
+var assets    = require('connect-assets');
+var path      = require('path');
+var compress  = require('compression');
+var log       = require('morgan');
 
 module.exports = function middleware(app) {
   // logging
   app.use(log(app.get('logType')));
 
+  // gzip
+  app.use(compress());
+
   // assets (SCSS, etc.)
-  app.use(assets());
+  app.use(assets({
+    buildDir: 'public'
+  }));
 };
