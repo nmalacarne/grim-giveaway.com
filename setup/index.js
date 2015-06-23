1921782c41ba01aa6138da0e4b0703124a690853
Nicholas Malacarne <nick@craterdust.com>
2015-06-23 14:05:32 -0400

    setup now sanitizes request body, params, and query

--- setup/index.js
+++ setup/index.js
@@ -3,6 +3,7 @@ var path      = require('path');
 var compress  = require('compression');
 var log       = require('morgan');
 var parser    = require('body-parser');
+var sanitize  = require('mongo-sanitize');
 var db        = require('mongoose');
 
 module.exports = function setup(app) {
@@ -20,6 +21,14 @@ module.exports = function setup(app) {
   // json parsing
   app.use(parser.json());
   app.use(parser.urlencoded({extended: true}));
+
+  app.use(function(req, res, next) {
+    req.body    = sanitize(req.body);
+    req.query   = sanitize(req.query);
+    req.params  = sanitize(req.params);
+
+    next();
+  });
 
   // mongo setup
   db.connect(process.env.DB_URL);
