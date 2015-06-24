d5b66e65c21aba7ecbb2529d69e60821747de75d
Nicholas Malacarne <nick@craterdust.com>
2015-06-24 11:33:43 -0400

    implemented home form; renamed assets for better clarity

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
-var home = require('./home');
-
 module.exports = function routes(app) {
-  app.get('/', home.get);
-  app.post('/', [home.checkParams, home.checkUnique], home.post);
+  app.get('/', function(req, res) { res.render('pages/home'); });
+
+  app.post('/', function(req, res) {
+    if (!req.body.profileName) res.render('pages/home', {error: 'Please enter a profile name.'});
+
+    res.send(req.body.profileName);
+  });
 
   // 404
   app.use(function PageNotFound(req, res) {
