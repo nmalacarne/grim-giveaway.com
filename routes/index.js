25576a353cffa6156981f672663e7ca8388f44d6
Nicholas Malacarne <nick@craterdust.com>
2015-06-25 15:07:18 -0400

    now check if entrant already owns the game

--- routes/index.js
+++ routes/index.js
@@ -14,23 +14,42 @@ module.exports = function routes(app) {
     },
     function indexPost(req, res) {
       Steam.ready(function(err) {
-        if (err) return res.render('pages/home', {error: 'An error has occurred; Please try again.'});
+        if (err) return res.render('pages/home', {error: 'An error has occurred; Please contact an administrator.'});
 
         var steam = new Steam();
 
-        steam.resolveVanityURL({vanityurl: req.body.profileName}, function(err, profile) {
-          if (err) return res.render('pages/home', {error: 'An error occurred; Please try again.'});
-
+        // lookup entrant's steam id
+        steam.resolveVanityURL({
+          vanityurl: req.body.profileName
+        }, function(err, profile) {
+          if (err) return res.render('pages/home', {error: 'An error occurred; Please contact an administrator.'});
+          // 1: success
+          // 46: user not found
           switch(profile.success) {
             case 1:
-              new Entrant({steam_id: profile.steamid}).save(function(err, entrant) {
-                if (err) return res.render('pages/home', {error: req.body.profileName + ' has already been entered into the contest.'});
-
-                steam.getPlayerSummaries({steamids: profile.steamid}, function(err, summary) {
-                  if (err) return res.render('pages/home', {error: 'An error has occurred; Please try again.'});
-                  console.log(summary.players);
-                  return res.render('pages/congrats', {
-                    user: summary.players.shift()
+              steam.getOwnedGames({
+                steamid                   : profile.steamid
+              , include_appinfo           : false
+              , include_played_free_games : false
+              , appids_filter             : null
+              }, function(err, library) {
+                // check if grim dawn is on list of owned games
+                for (var i = 0; i < library.games.length; i++) {
+                  if (library.games[i].appid === 219990) return res.render('pages/home', {error: req.body.profileName + ' already owns a copy of Grim Dawn.'});
+                }
+                // create new entrant
+                new Entrant({
+                  steam_id: profile.steamid
+                }).save(function(err, entrant) {
+                  if (err) return res.render('pages/home', {error: req.body.profileName + ' has already been entered into the contest.'});
+                  // query steam api for user profile data (name, icon, etc)
+                  steam.getPlayerSummaries({
+                    steamids: profile.steamid
+                  }, function(err, summary) {
+                    if (err) return res.render('pages/home', {error: 'An error has occurred; Please contact an administrator.'});
+                    return res.render('pages/congrats', {
+                      user: summary.players.shift()
+                    });
                   });
                 });
               });
