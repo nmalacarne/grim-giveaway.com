bbc721203fc1c045fdcdabb73befe5458df8d469
Nicholas Malacarne <nick@craterdust.com>
2015-07-01 15:23:27 -0400

    added created at and updated at fields to entrant model

--- models/entrant.js
+++ models/entrant.js
@@ -7,6 +7,18 @@ var entrantSchema = new Schema({
   , profile_url   : { type: String, required: true }
   , icon_url      : { type: String, required: true }
   , winner        : { type: Boolean, default: false  }
+  , created_at    : { type: Date }
+  , updated_at    : { type: Date }
+});
+
+entrantSchema.pre('save', function(next) {
+  var now = new Date();
+
+  this.updated_at = now;
+
+  if (!this.created_at) this.created_at = now;
+
+  next();
 });
 
 module.exports = mongoose.model('Entrant', entrantSchema);
