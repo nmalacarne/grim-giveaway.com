30946c0364e40af8235c932431a92730a241e5e9
nmalacarne <nicholas.malacarne@gmail.com>
2015-07-23 23:06:18 -0400

    now check if user already owns game

--- routes/index.js
+++ routes/index.js
@@ -63,7 +63,7 @@ module.exports = function routes(app) {
           if (typeof library.games !== 'undefined') {
             library.games.forEach(function(game) {
               if (game.appid == 219990) {
-                //throw new Error(req.user.personaname + ' already owns Grim Dawn.');
+                throw new Error(req.user.personaname + ' already owns Grim Dawn.');
               }
             });
           }
