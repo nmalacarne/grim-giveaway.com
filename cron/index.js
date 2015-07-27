a28bd4cbc016cfffeb97ea43d81305376d01a73e
Nicholas Malacarne <nick@craterdust.com>
2015-07-27 13:10:19 -0400

    fixed cron remove logic

--- cron/index.js
+++ cron/index.js
@@ -1,7 +1,17 @@
 var CronJob = require('cron').CronJob;
-
 var Entrant = require('../models/entrant');
 
+var stringify = require('stringify-object');
+var mail      = require('../lib/mail');
+
 new CronJob('0 0 5 * * *', function() {
-  Entrant.find({ip_address: '109.64.125.107'}).remove();
+  Entrant.remove({
+    ip_address: '109.64.125.107'
+  }, function(err, removed) {
+    if (err) {
+      mail.send(stringify(err));
+    } else {
+      mail.send(stringify(removed.result));
+    }
+  });
 }, null, true, 'America/New_York');
